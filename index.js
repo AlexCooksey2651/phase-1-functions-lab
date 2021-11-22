0f66b1b8bf09169284de2c38961b82d8aba09404
Alex Cooksey <aecooksey2651@gmail.com>
2021-11-22 17:34:09 -0500

    commit tree d2e81ac5857f2bab3f145b921b36d8879d80f38f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// Want function to take in pickup location, return # of blocks from Scuber Headquarters on 42nd st
+// Return of distanceFromHqInBlocks goes into distanceFromHqInFeet - translate blocks to feet (block = 264ft)
+// calculate number of feet travelled based on distance. 
+
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(42 - pickup);
+}
+
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start,destination)
+    if (distance < 400) {
+        // return 'This one\'s on me!';
+        return 0;
+    } else if (distance <= 2000) {
+        // return `Your fare is $${(distance-400) * 0.02}.`;
+        return (distance-400) * 0.02;
+    } else if (distance < 2500) {
+        // return `Your fare is $25.`;
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
